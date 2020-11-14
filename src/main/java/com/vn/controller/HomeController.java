0cc8b4a9ffe4d93ba910a0e47817bee1ad27fcba
Huy Pham <ngochuy.mmt@gmail.com>
2020-11-14 22:34:51 +0700

    add code demo

--- src/main/java/com/vn/controller/HomeController.java
+++ src/main/java/com/vn/controller/HomeController.java
@@ -1,42 +1,14 @@
 package com.vn.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.vn.model.Product;
-
-@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping(value= {"/home", "", "/" , "/index"})
-	public List<Product> getProduct() {
-		System.out.println("1");
-		// fake data
-		Product product = new Product();
-		product.setName("Test");
-		ArrayList<Product> arrayList = new ArrayList<Product>();
-		arrayList.add(product);
-		arrayList.add(product);
-		arrayList.add(product);
-		
-		return arrayList;
+	public String getProduct() {
+		return "index";
 	}
 
-	@PostMapping("/home")
-	public List<Product> getProduct2() {
-		System.out.println("3");
-		// fake data
-		Product product = new Product();
-		product.setName("Test");
-		ArrayList<Product> arrayList = new ArrayList<Product>();
-		arrayList.add(product);
-		arrayList.add(product);
-		arrayList.add(product);
-		
-		return arrayList;
-	}
 }
